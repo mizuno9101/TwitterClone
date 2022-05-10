18dd5cb0c59e31ed322d6e3213b083534e09dae0
mizuno9101 <mizuno9101@gmail.com>
2022-05-10 18:44:33 +0900

    ログイン機能

--- Models/users.php
+++ Models/users.php
@@ -21,29 +21,88 @@ function createUser(array $data)
         exit;
     }
 
-    // 新規登録のSQLクエリを作成
-    $query = 'INSERT INTO users (email, name, nickname, password) VALUES (?, ?, ?, ?)';
+        // 新規登録のSQLクエリを作成
+        $query = 'INSERT INTO users (email, name, nickname, password) VALUES (?, ?, ?, ?)';
 
-    // プリペアドステートメントに、作成したクエリを登録
-    $statement = $mysqli->prepare($query);
+        // プリペアドステートメントに、作成したクエリを登録
+        $statement = $mysqli->prepare($query);
+    
+        // パスワードをハッシュ値に変換
+        $data['password'] = password_hash($data['password'], PASSWORD_DEFAULT);
+    
+        // クエリのプレースホルダ（？の部分）にカラム値を紐づけ
+        $statement->bind_param('ssss', $data['email'], $data['name'], $data['nickname'], $data['password']);
+    
+        // クエリを実行
+        $response = $statement->execute();
+    
+        // 実行に失敗した場合->エラー表示
+        if ($response === false){
+            echo 'エラーメッセージ: ' . $mysqli->error . "\n";
+        }
+    
+        // DB接続を解放
+        $statement->close();
+        $mysqli->close();
+    
+        return $response;
+    }
+    
+    /**
+     * ユーザー情報取得: ログインチェック
+     * 
+     * @param string $email
+     * @param string $password
+     * @return array|false
+     */
 
-    // パスワードをハッシュ値に変換
-    $data['password'] = password_hash($data['password'], PASSWORD_DEFAULT);
+     function findUserAndCheckPassword(string $email, string $password)
+     {
+         // DB接続
+         $mysqli = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+         
+         // 接続エラーがある場合->処理停止
+         if ($mysqli->connect_errno){
+             echo 'MySQLの接続に失敗しました。 :' . $mysqli->conect_error . "\n";
+             exit;
+         
+     }
 
-    // クエリのプレースホルダ（？の部分）にカラム値を紐づけ
-    $statement->bind_param('ssss', $data['email'], $data['name'], $data['nickname'], $data['password']);
+    // 入力値をエスケープ
+    $email = $mysqli->real_escape_string($email);
 
-    // クエリを実行
-    $response = $statement->execute();
+    // SQLクエリを作成
+    // 外部からのリクエストは何が入ってくるかわからないので、必ずエスケープしたものをクオートで囲む
+    $query = 'SELECT * FROM users WHERE email = "' . $email . '"';
 
-    // 実行に失敗した場合->エラー表示
-    if ($response === false){
-        echo 'エラーメッセージ: ' . $mysqli->error . "\n";
+    // クエリ実行
+    $result = $mysqli->query($query);
+
+    // クエリ実行に失敗した場合->return
+    if (!$result){
+        //MySQL処理中にエラー発生
+        echo 'エラーメッセージ:' . $mysqli->error . "\n";
+        $mysqli->close();
+        return false;
+    }
+
+    // ユーザー情報を取得
+    $user = $result->fetch_array(MYSQLI_ASSOC);
+    // ユーザーが存在しない場合->return
+    if(!$user){
+        $mysqli->close();
+        return false;
+    }
+
+    // パスワードチェック、不一致の場合->return
+    if(!password_verify($password, $user['password'])){
+        $mysqli-close();
+        return false;
     }
 
     // DB接続を解放
-    $statement->close();
     $mysqli->close();
-
-    return $response;
+ 
+    return $user;
 }
+
--- Views/home.php
+++ Views/home.php
@@ -1,41 +1,3 @@
-<?php
-// 設定関連を読み込む
-include_once('../config.php');
-// 便利な関数を読み込む
-include_once('../util.php');
-
-///////////////////////////////////////
-// ツイート一覧
-//////////////////////////////////////
-$view_tweets = [
-    [
-        'user_id' => 1,
-        'user_name' => 'taro',
-        'user_nickname' => '太郎',
-        'user_image_name' => 'sample-person.jpg',
-        'tweet_body' => '今プログラミングをしています。',
-        'tweet_image_name' => null,
-        'tweet_created_at' => '2022-04-15 14:00:00',
-        'like_id' => null,
-        'like_count' => 0,
-    ],
-    [ 
-        'user_id' => 2,
-        'user_name' => 'jiro',
-        'user_nickname' => '次郎',
-        'user_image_name' => null,
-        'tweet_body' => 'コワーキングスペースをオープンしました！',
-        'tweet_image_name' => 'sample-post.jpg',
-        'tweet_created_at' => '2021-03-14 14:00:00',
-        'like_id' => 1,
-        'like_count' => 1,
-    ]
-];
-
-?>
-
-
-
 <!DOCTYPE html>
 <html lang="ja">
  
@@ -52,14 +14,14 @@ $view_tweets = [
             <div class="main-header">
                 <h1>ホーム</h1>
             </div>
-
-            <!--つぶやき投稿エリア-->
+ 
+            <!-- つぶやき投稿エリア -->
             <div class="tweet-post">
                 <div class="my-icon">
-                    <img src="<?php echo HOME_URL;?>Views/img_uploaded/user/sample-person.jpg" alt="">
+                    <img src="<?php echo HOME_URL; ?>Views/img_uploaded/user/sample-person.jpg" alt="">
                 </div>
                 <div class="input-area">
-                    <form action="post.php" method="post" enctype="multipart/form-date">
+                    <form action="post.php" method="post" enctype="multipart/form-data">
                         <textarea name="body" placeholder="いまどうしてる？" maxlength="140"></textarea>
                         <div class="bottom-area">
                             <div class="mb-0">
@@ -70,19 +32,20 @@ $view_tweets = [
                     </form>
                 </div>
             </div>
-            <!--仕切りエリア-->
+ 
+            <!-- 仕切りエリア -->
             <div class="ditch"></div>
-
-            <!--つぶやき一覧エリア-->
-<?php if (empty($view_tweets)) : ?>
-    <p class="p-3">ツイートがありません</p>
-<?php else : ?>
-        <div class="tweet-list">
-            <?php foreach($view_tweets as $view_tweet): ?>
-                <?php include('../Views/common/tweet.php'); ?>    
-                <?php endforeach; ?>
-            </div>
-        <?php endif; ?>
+ 
+            <!-- つぶやき一覧エリア -->
+            <?php if (empty($view_tweets)) : ?>
+                <p class="p-3">ツイートがありません</p>
+            <?php else : ?>
+                <div class="tweet-list">
+                    <?php foreach ($view_tweets as $view_tweet) : ?>
+                        <?php include('../Views/common/tweet.php'); ?>
+                    <?php endforeach; ?>
+                </div>
+            <?php endif; ?>
         </div>
     </div>
     <?php include_once('../Views/common/foot.php'); ?>
